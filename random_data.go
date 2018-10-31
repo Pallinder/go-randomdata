94a7baf2828df71ef2486ccb43f5e9cd2a15de18
Mark Songhurst <MarkSonghurst@users.noreply.github.com>
2018-10-31 23:08:01 +0300

    Support for Country localised street names and provinces
    Background
    This change enables support for Country localised street names and provinces.
    
    Caveats 
    The change only supports Great Britain (GB) and US at the moment. Other countries can be added easily enough.
    
    Changes 
    * Constant JSON data added for GB provinces and street names.
    * Function ProvinceForCountry added
    * Function StreetForCountry added
    * Test cases added.

--- random_data.go
+++ random_data.go
@@ -60,6 +60,9 @@ type jsonContent struct {
 	Locales             []string `json:"locales"`             // https://tools.ietf.org/html/bcp47
 	UserAgents          []string `json:"userAgents"`          // http://techpatterns.com/downloads/firefox/useragentswitcher.xml
 	CountryCallingCodes []string `json:"countryCallingCodes"` // from https://github.com/datasets/country-codes/blob/master/data/country-codes.csv
+	ProvincesGB         []string `json:"provincesGB"`
+	StreetNameGB        []string `json:"streetNameGB"`
+	StreetTypesGB       []string `json:"streetTypesGB"`
 }
 
 var jsonData = jsonContent{}
@@ -166,6 +169,18 @@ func City() string {
 	return randomFrom(jsonData.Cities)
 }
 
+// ProvinceForCountry returns a randomly selected province (state, county,subdivision ) name for a supplied country.
+// If the country is not supported it will return an empty string.
+func ProvinceForCountry(countrycode string) string {
+	switch countrycode {
+	case "US":
+		return randomFrom(jsonData.States)
+	case "GB":
+		return randomFrom(jsonData.ProvincesGB)
+	}
+	return ""
+}
+
 // State returns a random american state
 func State(typeOfState int) string {
 	if typeOfState == Small {
@@ -177,6 +192,18 @@ func State(typeOfState int) string {
 // Street returns a random fake street name
 func Street() string {
 	return fmt.Sprintf("%s %s", randomFrom(jsonData.People), randomFrom(jsonData.StreetTypes))
+}
+
+// StreetForCountry returns a random fake street name typical to the supplied country.
+// If the country is not supported it will return an empty string.
+func StreetForCountry(countrycode string) string {
+	switch countrycode {
+	case "US":
+		return Street()
+	case "GB":
+		return fmt.Sprintf("%s %s", randomFrom(jsonData.StreetNameGB), randomFrom(jsonData.StreetTypesGB))
+	}
+	return ""
 }
 
 // Address returns an american style address
